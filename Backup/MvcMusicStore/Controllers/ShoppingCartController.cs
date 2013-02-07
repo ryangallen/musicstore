7ab9f8d67ce20e3ea247a36782bfacae97679a97
Ryan <allenryan14@gmail.com>
2013-02-07 14:12:12 -0500

    Through part 8 - Shopping cart finished.

---
+++ Backup/MvcMusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using MvcMusicStore.ViewModels;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        //
+        // GET: /ShoppingCart/
+
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            //set up viewmodel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+            //return the view
+            return View(viewModel);
+        }
+        //GET: /Store/AddToCart/5
+        public ActionResult AddToCart(int id) { 
+            //retrieve album from db
+            var addedAlbum = storeDB.Albums
+                .Single(album => album.AlbumId == id);
+
+            //Add it to the cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            cart.AddToCart(addedAlbum);
+
+            //Go back to the main store page for more shopping
+            return RedirectToAction("Index");
+        }
+        //
+        //AJAX: /ShoppingCart/RemoveFromCart/5
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id) { 
+            //remove the item from the cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            //Get the name of the album to display confirmation
+            string albumName = storeDB.Carts
+                .Single(item => item.RecordId == id).Album.Title;
+            //remove from cart
+            int itemCount = cart.RemoveFromCart(id);
+
+            //display the confirmation message
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(albumName) + "has been removed from your cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+            return Json(results);
+        }
+
+        //GET: /ShoppingCart/CartSummary
+        [ChildActionOnly]
+        public ActionResult CartSummary() {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+    }
+}
---
+++ Backup/MvcMusicStore/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMusicStore.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int RecordId { set; get; }
+        public string CartId { set; get; }
+        public int AlbumId { set; get; }
+        public int Count { set; get; }
+        public System.DateTime DateCreated { set; get; }
+        public virtual Album Album { set; get; }
+    }
+}
